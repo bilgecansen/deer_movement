48179a4c6d942aae77843beb9aaa896131d92372
bilgecan <bilgecan.sen@gmail.com>
2026-02-26 15:13:02 -0500

    add wiscland, new wrapped function, parallelization

--- fit_issf.R
+++ fit_issf.R
@@ -4,6 +4,8 @@
 #' 2. Create random steps with environmental covariates
 #' 3. Fit iSSF models
 #' 4. Simulate movement from models
+#' 5. Estimate and compare utilization distributions
+#' 6. Estimate proximity
 
 # Load packages ----------------------------------------------------------------
 library(amt)
@@ -11,6 +13,7 @@ library(terra)
 library(tidyverse)
 library(foreach)
 library(sf)
+library(doParallel)
 
 
 # Load data --------------------------------------------------------------------
@@ -18,6 +21,12 @@ env_rasters <- list(
   env_2017 = rast('Example_code/Env_2017.tif'),
   env_2018 = rast('Example_code/Env_2018.tif')
 )
+
+env_raster <- rast("env/wiscland/wiscland2.tif")
+names(env_raster) <- "wiscland"
+
+water_binary <- ifel(env_raster == "water", 1, 0)
+names(water_binary) <- "Water"
 
 # Load NDVI data
 ndvi_rasters <- list(
@@ -38,7 +47,7 @@ deer_mvt <- raw_data %>%
 #' @param data Dataframe with movement steps
 #' @param n_pts Number of random points per step
 #' @param Env_list List of environmental rasters by year
-make_random_pt_extraction <- function(data, n_pts, env_list = env_rasters) {
+make_random_pt_extraction <- function(data, n_pts, water = water_binary) {
   random.stp <- foreach(i = 1:nrow(data)) %do%
     {
       print(i)
@@ -46,7 +55,7 @@ make_random_pt_extraction <- function(data, n_pts, env_list = env_rasters) {
       z <- data[i, ]
 
       # Extract water layer for this year
-      water <- env_list[[paste0('env_', z$year)]]$Water
+      #water <- env_list[[paste0('env_', z$year)]]$Water
       data_step <- z$stp[[1]]
 
       # Start with buffer for water removal
@@ -98,13 +107,13 @@ make_random_pt_extraction <- function(data, n_pts, env_list = env_rasters) {
 #' @param ndvi_list NDVI rasters
 extract_step_variables <- function(
   data,
-  env_list = env_rasters,
+  env = env_raster,
   ndvi_list = ndvi_rasters
 ) {
   stp.var <- foreach(i = 1:nrow(data)) %do%
     {
       data_row <- data[i, ]
-      env <- env_list[[paste0('env_', data_row$year)]]
+      #env <- env_list[[paste0('env_', data_row$year)]]
 
       # Add HR distance to environment
       median_pt <- vect(
@@ -114,7 +123,7 @@ extract_step_variables <- function(
 
       ## Any layer in env works for distance calculation
       env$HR <- NA
-      env$HR <- values(terra::distance(env$ele, median_pt)) / 1000
+      env$HR <- values(terra::distance(env$wiscland, median_pt)) / 1000
 
       ndvi <- ndvi_list[[paste0('ndvi_', data_row$year)]]
 
@@ -130,13 +139,31 @@ extract_step_variables <- function(
         ) %>%
         # Setting the intercept to the 'forest' land type
         mutate(
-          Coarse_scale_end = factor(
-            Coarse_scale_end,
-            levels = c("forest", 'agriculture', 'grassland', 'other')
+          wiscland_start = factor(
+            wiscland_start,
+            levels = c(
+              "forest",
+              'agriculture',
+              'grassland',
+              "hay",
+              "oak",
+              "central.hardwoods",
+              "other.forest,",
+              'other'
+            )
           ),
-          Coarse_scale_start = factor(
-            Coarse_scale_start,
-            levels = c("forest", 'agriculture', 'grassland', 'other')
+          wiscland_end = factor(
+            wiscland_end,
+            levels = c(
+              "forest",
+              'agriculture',
+              'grassland',
+              "hay",
+              "oak",
+              "central.hardwoods",
+              "other.forest,",
+              'other'
+            )
           )
         )
       data_ssf
@@ -172,13 +199,13 @@ fit_multi_mods <- function(data, var, formula) {
 }
 
 #' Simulate a single movement path
-#' @param deer_test Step data for testing sims
+#' @param deer_data Deer movement data (e.g., deer_mvt)
 #' @param env_test Environmental rasters for test data
 #' @param ndvi_test ndvi rasters for test data
 #' @param issf_train Fitted iSSF model
 #' @param formula_name Formula identifier
 simulate_movement <- function(
-  deer_test,
+  deer_data,
   env_test,
   ndvi_test,
   issf_train,
@@ -186,7 +213,7 @@ simulate_movement <- function(
 ) {
   # Calculate distance to home range center
   median_pt <- terra::vect(
-    cbind(deer_test$x_median, deer_test$y_median),
+    cbind(deer_data$x_median, deer_data$y_median),
     crs = crs(env_test[[1]])
   )
 
@@ -201,7 +228,7 @@ simulate_movement <- function(
     )
   )
 
-  sim_days <- unique(as.Date(deer_test$stp[[1]]$t1_, tz = 'CST6CDT'))
+  sim_days <- unique(as.Date(deer_data$stp[[1]]$t1_, tz = 'CST6CDT'))
 
   sim_days_all <- seq.Date(
     first(sim_days),
@@ -267,6 +294,145 @@ simulate_movement <- function(
   sim_i[-1, ]
 }
 
+#' Run simulate_movement across all deer and replications for a given model
+#' @param deer_data Deer movement data (e.g., deer_mvt)
+#' @param model_results List of model results from fitting step
+#' @param env Environment raster to pass to simulate_movement
+#' @param ndvi_list Named list of NDVI rasters
+#' @param formula_df Dataframe of formulas
+#' @param n_deer Number of deers to simulate
+#' @param n_sim Number of simulations per deer
+run_simulations <- function(
+  deer_data,
+  model_results,
+  env,
+  ndvi_list,
+  formula_df,
+  n_deer = nrow(deer_data),
+  n_sim = 10
+) {
+  # Wrap terra rasters so they can be sent to parallel workers
+  env_wrapped <- terra::wrap(env)
+  ndvi_wrapped <- lapply(ndvi_list, terra::wrap)
+
+  foreach(
+    i = 1:n_deer,
+    .packages = c("amt", "terra", "sf", "dplyr", "foreach"),
+    .export = c("simulate_movement")
+  ) %dopar%
+    {
+      # Unwrap rasters inside the worker
+      env_local <- terra::unwrap(env_wrapped)
+      ndvi_local <- lapply(ndvi_wrapped, terra::unwrap)
+
+      coefs <- model_results$iss[[i]]$model$coefficients
+      coefs <- coefs[!is.na(coefs)]
+
+      model_sim <- make_issf_model(
+        coefs = coefs,
+        sl = model_results$iss[[i]]$sl_,
+        ta = model_results$iss[[i]]$ta_
+      )
+
+      foreach(h = 1:n_sim, .combine = "rbind") %do%
+        {
+          res <- simulate_movement(
+            deer_data[i, ],
+            env_local,
+            ndvi_local[[paste("ndvi", deer_data[i, ]$year, sep = "_")]],
+            model_sim,
+            formula_df
+          )
+          res$nsim <- h
+          res
+        }
+    }
+}
+
+
+#' Estimate overlap of utilization distributions
+#' @param data Observed paths
+#' @param sim Simulated paths
+#' @param n_sim number of simulated paths
+overlap_ud <- function(data, sim, n_sim) {
+  z1 <- data |>
+    select("x1_", "y1_", "t1_") |>
+    st_as_sf(coords = c("x1_", "y1_"), crs = 6610) |>
+    st_transform(4326) |> # Transform to WGS84 lat/long
+    mutate(
+      longitude = st_coordinates(geometry)[, 1],
+      latitude = st_coordinates(geometry)[, 2],
+      timestamp = t1_
+    ) |>
+    st_drop_geometry() |>
+    as.data.frame() |>
+    as.telemetry()
+
+  z2 <- foreach(k = 1:n_sim) %do%
+    {
+      tel <- sim |>
+        filter(nsim == k) |>
+        select("x_", "y_", "t_") |>
+        st_as_sf(coords = c("x_", "y_"), crs = 6610) |>
+        st_transform(4326) |> # Transform to WGS84 lat/long
+        mutate(
+          longitude = st_coordinates(geometry)[, 1],
+          latitude = st_coordinates(geometry)[, 2],
+          timestamp = t_
+        ) |>
+        st_drop_geometry() |>
+        as.data.frame() |>
+        as.telemetry()
+
+      projection(tel) <- projection(z1)
+
+      tel
+    }
+
+  # Select best ctmm for observed and simulated paths
+  ms1 <- ctmm.select(z1, ctmm.guess(z1), verbose = TRUE, cores = 4)
+  ms2 <- foreach(k = 1:length(z2)) %do%
+    {
+      ctmm.select(z2[[k]], ctmm.guess(z2[[k]]), verbose = TRUE, cores = 4)
+    }
+
+  # Estimate UD
+  uds <- foreach(k = 1:length(z2)) %do%
+    {
+      akde(list(z1, z2[[k]]), list(ms1[[1]], ms2[[k]][[1]]))
+    }
+
+  # Estiamte the overlap of UDs
+  bat <- map_dbl(uds, function(x) overlap(x)$CI[1, 2, 2])
+
+  list(
+    data = z1,
+    sim = z2,
+    ctmm_data = ms1[[1]],
+    ctmm_sim = ms2,
+    uds = uds,
+    bat_coeff = bat
+  )
+}
+
+#' Estimate proximity between observed and a simulated path
+#' @param res_ud output of overlap_ud function
+#' @param n_sim Number of simulated paths
+prox_path <- function(res_ud, n_sim) {
+  z1 <- res_ud$data
+  z2 <- res_ud$sim
+
+  prox <- foreach(k = 1:10, .combine = "rbind") %do%
+    {
+      proximity(
+        list(z1, z2[[k]]),
+        list(res_ud$ctmm_data, res_ud$ctmm_sim[[k]][[1]])
+      )
+    }
+
+  c(mean(prox[, 2]), length(which(prox[, 3] < 1)))
+}
+
 # Main workflow ----------------------------------------------------------------
 
 # Set seed for reproducibility
@@ -277,7 +443,7 @@ cat("Generating random steps...\n")
 deer_with_random <- make_random_pt_extraction(
   data = deer_mvt,
   n_pts = 10,
-  env_list = env_rasters
+  water = water_binary
 ) %>%
   select(id, season, year, age.at.col1, sex, indicator, random.stp)
 
@@ -295,12 +461,15 @@ deer_mvt_var <-
 
 # Step 3: Fit models
 cat("Fitting models...\n")
+null <- "case_ ~ log(sl_) + cos(ta_) + HR_end + strata(step_id_)"
 formula_df <- data.frame(
   formula = c(
-    "case_ ~ log(sl_) + cos(ta_) + HR_end + strata(step_id_)",
-    "case_ ~ log(sl_) + cos(ta_) + HR_end + ndvi_end + strata(step_id_)"
+    null,
+    paste(null, "+ ndvi_end"),
+    paste(null, "+ wiscland_end"),
+    paste(null, "+ wiscland_end*ndvi_end")
   ),
-  name = c("null_model_lin", "ndvi")
+  name = c("null", "ndvi", "wiscland", "wisc*ndvi")
 )
 
 ## Fit models for each formula
@@ -326,60 +495,62 @@ model_res <- foreach(i = 1:nrow(formula_df)) %do%
 # Step 4: Simulate movement
 cat("Simulating movement...\n")
 
-sim_res_hr <- foreach(i = 1:10) %do%
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+tic()
+sim_res_hr <- run_simulations(
+  deer_mvt,
+  model_res[[1]],
+  env_raster,
+  ndvi_rasters,
+  formula_df[1, 1]
+)
+toc()
+
+sim_res_ndvi <- run_simulations(
+  deer_mvt,
+  model_res[[2]],
+  env_raster,
+  ndvi_rasters,
+  formula_df[2, 1]
+)
+
+sim_res_wisc <- run_simulations(
+  deer_mvt,
+  model_res[[3]],
+  env_raster,
+  ndvi_rasters,
+  formula_df[3, 1]
+)
+
+# Step 5: Estimate and compare UD
+cat("Estimating overlap of UDs...\n")
+
+res_ud_hr <- foreach(i = 1:10) %do%
   {
-    coefs <- model_res[[1]]$iss[[i]]$model$coefficients
-    coefs <- coefs[!is.na(coefs)]
-
-    model_sim <- make_issf_model(
-      coefs = coefs,
-      sl = model_res[[1]]$iss[[i]]$sl_,
-      ta = model_res[[1]]$iss[[i]]$ta_
-    )
-
-    foreach(h = 1:10, .combine = "rbind") %do%
-      {
-        res <- simulate_movement(
-          deer_mvt[i, ],
-          env_rasters[[1]],
-          ndvi_rasters[[paste("ndvi", deer_mvt[i, ]$year, sep = "_")]],
-          model_sim,
-          formula_df[1, 1]
-        )
-
-        res$nsim <- h
-
-        res
-      }
+    overlap_ud(deer_mvt$stp[[i]], sim_res_hr[[i]], n_sim = 10)
   }
 
-sim_res_ndvi <- foreach(i = 1:10) %do%
+res_ud_ndvi <- foreach(i = 1:10) %do%
   {
-    coefs <- model_res[[2]]$iss[[i]]$model$coefficients
-    coefs <- coefs[!is.na(coefs)]
-
-    model_sim <- make_issf_model(
-      coefs = coefs,
-      sl = model_res[[2]]$iss[[i]]$sl_,
-      ta = model_res[[2]]$iss[[i]]$ta_
-    )
-
-    foreach(h = 1:10, .combine = "rbind") %do%
-      {
-        res <- simulate_movement(
-          deer_mvt[i, ],
-          env_rasters[[1]],
-          ndvi_rasters[[paste("ndvi", deer_mvt[i, ]$year, sep = "_")]],
-          model_sim,
-          formula_df[2, 1]
-        )
-
-        res$nsim <- h
-
-        res
-      }
+    overlap_ud(deer_mvt$stp[[i]], sim_res_ndvi[[i]], n_sim = 10)
   }
 
+# Step 6: Estimate proximity between observed vs simulated paths
+cat("Estimating proximity...\n")
+
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+prox_ndvi <- foreach(i = 1:10, .combine = "rbind", .packages = "ctmm") %dopar%
+  {
+    prox_path(res_ud_ndvi[[i]], n_sim = 10)
+  }
+
+stopCluster(cl)
+
+# Plots ------------------------------------------------------------------------
 ## Visualize results
 cat("Creating visualization...\n")
 library(patchwork)
@@ -478,68 +649,3 @@ ggplot() +
     color = "orange",
     alpha = 0.1
   )
-
-# Calculate overlap of utilization distributions
-overlap_ud <- function(data, sim, n_sim) {
-  z1 <- data |>
-    select("x1_", "y1_", "t1_") |>
-    st_as_sf(coords = c("x1_", "y1_"), crs = 6610) |>
-    st_transform(4326) |> # Transform to WGS84 lat/long
-    mutate(
-      longitude = st_coordinates(geometry)[, 1],
-      latitude = st_coordinates(geometry)[, 2],
-      timestamp = t1_
-    ) |>
-    st_drop_geometry() |>
-    as.data.frame() |>
-    as.telemetry()
-
-  z2 <- foreach(i = 1:n_sim) %do%
-    {
-      tel <- sim |>
-        filter(nsim == i) |>
-        select("x_", "y_", "t_") |>
-        st_as_sf(coords = c("x_", "y_"), crs = 6610) |>
-        st_transform(4326) |> # Transform to WGS84 lat/long
-        mutate(
-          longitude = st_coordinates(geometry)[, 1],
-          latitude = st_coordinates(geometry)[, 2],
-          timestamp = t_
-        ) |>
-        st_drop_geometry() |>
-        as.data.frame() |>
-        as.telemetry()
-
-      projection(tel) <- projection(z1)
-
-      tel
-    }
-
-  ms1 <- ctmm.select(z1, ctmm.guess(z1), verbose = TRUE, cores = 4)
-
-  ms2 <- foreach(i = 1:length(z2)) %do%
-    {
-      ctmm.select(z2[[i]], ctmm.guess(z2[[i]]), verbose = TRUE, cores = 4)
-    }
-
-  uds <- foreach(i = 1:length(z2)) %do%
-    {
-      akde(list(z1, z2[[i]]), list(ms1[[1]], ms2[[i]][[1]]))
-    }
-
-  bat <- map_dbl(uds, function(x) overlap(x)$CI[1, 2, 2])
-
-  list(
-    data = z1,
-    sim = z2,
-    ctmm_data = ms1,
-    ctmm_sim = ms2,
-    uds = uds,
-    bat_coeff = bat
-  )
-}
-
-res_ud_hr <- foreach(i = 1:10) %do%
-  {
-    overlap_ud(deer_mvt$stp[[i]], sim_res_hr[[i]], n_sim = 10)
-  }
